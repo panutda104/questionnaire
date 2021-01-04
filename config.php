20ec5c9d76f2b52d3d02b16091833d831066bcdc
th.thongyang <th.thongyang@gmail.com>
2021-01-04 15:40:19 +0700

    update

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php 
-	$con=mysqli_connect("localhost","root","") or die ("Could not connect");
+	$con=mysqli_connect("localhost","root","123456") or die ("Could not connect");
 	mysqli_select_db($con,"dog_cat")or die ("Could not Select DB!!");
 	mysqli_query($con,"SET NAMES UTF8");
 	mysqli_query($con,"SET Character_set_result=UTF8");
