2d2e4ee596fe2cc27cae53076cc1ec155df73f0c
krittikornthammabut <krittikorn07@hotmail.com>
2021-01-05 14:56:43 +0700

    update

--- index.php
+++ index.php
@@ -1,41 +1,55 @@
+<style type="text/css">
+  .hiddenform {
+    display: none;
+  }
+</style>
+<script type="text/javascript">
+  function showForm(flag) {
+    document.getElementById('myDIV').style.display = flag === 1 ? 'block' : 'none';
+  }
+</script>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css\style_index.css">
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@300&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css\style_index.css">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@300&display=swap" rel="stylesheet">
 
-    <title>index</title>
+  <title>index</title>
 </head>
 
 <body>
-<section id="page1" class="page1">
+  <form id="page1" class="page1" action="personalinformation.php" method="POST">
 
-                <div class="text_header_index">
-                        แบบสอบถาม เรื่อง การเก็บรวมรวบข้อมูลอาชีพที่เหมาะสมกับทักษะ ความรู้ และเจตคติ 
-                        ของผู้ประกอบอาชีพด้านเทคโนโลยีดิจิทัลหรือนิเทศศาสตร์ดิจิทัล
-                </div>
-                <div class="index">
-                    <div>
-                        <p id="text_header_index">คำชี้แจง</p>
-                        <img src="images\1111.jpg">
-                  
-                        <p id="text_header_index"><input type="checkbox" name="remember"> การยินยอมเข้าร่วมวิจัย</input></p>
-                    
-                        <i><p id="text_content1_index">ข้าพเจ้าได้อ่านแบบคำชี้แจงเกี่ยวกับการเข้าร่วมการเป็นผู้ตอบแบบสอบถามในโครงการวิจัย เรื่อง “การพัฒนาแบบจำลองการแนะนำวิชาชีพด้านดิจิทัลสำหรับหลักสูตรฐานสมรรถนะรายบุคคล” ซึ่งประกอบด้วย วัตถุประสงค์ของการวิจัย ประโยชน์โดยตรงที่จะได้รับจากการเข้าร่วมโครงการวิจัยในครั้งนี้ ขั้นตอนการปฏิบัติตัว ตลอดจนการรับรองจากผู้วิจัยที่จะเก็บรักษาข้อมูลในการตอบแบบสอบถามของข้าพเจ้าไว้เป็นความลับ และ ไม่ระบุชื่อหรือข้อมูลส่วนตัวเป็นรายบุคคลต่อสาธารณชน โดยผลการวิจัยจะนำเสนอในลักษณะภาพรวมที่เป็นการสรุปผลการวิจัยเพื่อประโยชน์ทางวิชาการเท่านั้น ข้าพเจ้าได้อ่านและเข้าใจตามคำอธิบายข้างต้นแล้ว จึงได้ลงนามยินยอมเข้าร่วมโครงการวิจัยนี้ด้วยความสมัครใจ</p></i>
-                       
-                        <input onclick="location.href='personalinformation.php'"  class="button"  type="submit" ac  name="Submit" value="ถัดไป">
+    <div class="text_header_index">
+      แบบสอบถาม เรื่อง การเก็บรวมรวบข้อมูลอาชีพที่เหมาะสมกับทักษะ ความรู้ และเจตคติ
+      ของผู้ประกอบอาชีพด้านเทคโนโลยีดิจิทัลหรือนิเทศศาสตร์ดิจิทัล
+    </div>
+    <div class="index">
+      <div>
+        <p id="text_header_index">คำชี้แจง</p>
+        <img src="images\1111.jpg">
 
-                    </div> 
-                </div>
-                
-                
-            </section>
+        <p id="text_header_index">
+          <input type="radio" name="checkbox_remember" onclick="showForm(1)"> การยินยอมเข้าร่วมวิจัย
+
+        </p>
+
+        <i>
+          <p id="text_content1_index">ข้าพเจ้าได้อ่านแบบคำชี้แจงเกี่ยวกับการเข้าร่วมการเป็นผู้ตอบแบบสอบถามในโครงการวิจัย เรื่อง “การพัฒนาแบบจำลองการแนะนำวิชาชีพด้านดิจิทัลสำหรับหลักสูตรฐานสมรรถนะรายบุคคล” ซึ่งประกอบด้วย วัตถุประสงค์ของการวิจัย ประโยชน์โดยตรงที่จะได้รับจากการเข้าร่วมโครงการวิจัยในครั้งนี้ ขั้นตอนการปฏิบัติตัว ตลอดจนการรับรองจากผู้วิจัยที่จะเก็บรักษาข้อมูลในการตอบแบบสอบถามของข้าพเจ้าไว้เป็นความลับ และ ไม่ระบุชื่อหรือข้อมูลส่วนตัวเป็นรายบุคคลต่อสาธารณชน โดยผลการวิจัยจะนำเสนอในลักษณะภาพรวมที่เป็นการสรุปผลการวิจัยเพื่อประโยชน์ทางวิชาการเท่านั้น ข้าพเจ้าได้อ่านและเข้าใจตามคำอธิบายข้างต้นแล้ว จึงได้ลงนามยินยอมเข้าร่วมโครงการวิจัยนี้ด้วยความสมัครใจ</p>
+        </i>
+        <div id="myDIV" class="hiddenform">
+          <input type='submit' class="button hiddenform" type="submit" value="ถัดไป">
+        </div>
+
+      </div>
+    </div>
+  </form>
 
 </html>
