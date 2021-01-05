437dc50724f26d3468aec35e3cf9a16146694c1a
panutda.2112@gmail.com <panutda.2112@gmail.com>
2021-01-05 17:03:05 +0700

    mhjkyu

---
+++ finish.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css\finish.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@300&display=swap" rel="stylesheet">
+
+    <title>shade</title>
+</head>
+
+<body>      
+            <section id="page1" class="page1">
+                <div class="shade">                   
+                </div>
+                <div class="shade1">             
+                    <div class="text_shade1">
+                    <div class="text_header_shade">คุณได้ทำแบบสอบถามเรียบร้อยแล้ว</div>
+                    <img src="images\finish.png  "width="300" height="300" >
+                </div> 
+
+                <button class=" button_shade1 next"> Exit</button>
+</section>              
+</body>
+</html>
